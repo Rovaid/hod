6951e1b8e1e1b282b5aeac696b1a3faedb64614c
Rovaid <37608893+Rovaid@users.noreply.github.com>
2021-06-14 19:44:37 +0500

    Add files via upload

---
+++ static/js/indexmain.js
@@ -0,0 +1,9 @@
+
+    (function () {
+        window['__CF$cv$params'] = {
+            r: '65e2baf0cf360a8e',
+            m: '71aeb69fbc4d8fa156939c83a2be00362a46d585-1623497249-1800-AXBfakHgoRRnov4awWwJR2IK9O16oHKfUMRxpHJWwo7EIIVo9qjwTELATf9boVMJg2l90R4JiiilTiHDFQLPJS4ptfn6ad1SrNwFZPxZXz19Rgb9B1dKO/zli3UYHz6X2A==',
+            s: [0x4b75ed9e13, 0x858397e789],
+        }
+    })();
+    
